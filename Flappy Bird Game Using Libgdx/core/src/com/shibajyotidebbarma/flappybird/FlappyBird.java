d288efa3c280b8c04d703ee40d4c10f4f045a17f
Shibajyoti Debbarma <debbarmaonit@gmail.com>
2017-09-09 17:28:07 +0530

    Initial Libgdx render and background sprites added

---
+++ Flappy Bird Game Using Libgdx/core/src/com/shibajyotidebbarma/flappybird/FlappyBird.java
@@ -0,0 +1,33 @@
+package com.shibajyotidebbarma.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("bg.png");
+
+
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+		batch.draw(background,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.end();
+
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		background.dispose();
+	}
+}
